6854934e28a5db40940ea95448dadbd2a74e0e65
Bai Chan Kheo <kheob@hotmail.com>
2016-09-23 15:53:59 +1000

    Hello

---
+++ server.js
@@ -0,0 +1,20 @@
+var johnny = require('johnny-five');
+var raspio = require('rasp-io');
+var express = require('express');
+
+var board = new five.Board({
+    io: new raspio()
+});
+
+board.on('ready', function() {
+    var multi = new five.Multi({
+        controller: "MPL3115A2",
+        elevation: 23
+    });
+
+    console.log("Server starting");
+
+    multi.on('change', function() {
+        console.log("Temp: ", this.thermometer.celsius, " Pressure: ", this.barometer.pressure, " Altitude: ", this.altimeter.meters);
+    });
+});
