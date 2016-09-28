15276d273e8a7e829a48ef0671d74ee309d9cc47
Bai Chan Kheo <kheob@hotmail.com>
2016-09-28 13:34:58 +1000

    Test

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
-var five = require('johnny-five');
-var raspio = require('raspi-io');
-var express = require('express');
+/**
+ * Application that starts the Raspberry Pi and the express server
+ *
+ * Author: Bai Chan Kheo
+ */
 
+var five = require('johnny-five'); // Source: http://johnny-five.io/
+var raspi = require('raspi-io'); // Source: https://github.com/nebrius/raspi-io
+var express = require('express'); // Source: https://expressjs.com/
+
+// Create a new board object passing in the raspi
 var board = new five.Board({
-    io: new raspio()
+    io: new raspi()
 });
 
 board.on('ready', function() {
+    // Create new object for the barometric sensor
     var multi = new five.Multi({
         controller: "MPL3115A2",
-        elevation: 23
+        elevation: 23 // Elevation from http://www.whatismyelevation.com
     });
 
-    console.log("Hello!");
+    // multi.on('change', function() {
+    //     console.log("Temp: ", this.thermometer.celsius, " Pressure: ", this.barometer.pressure, " Altitude: ", this.altimeter.meters);
+    // });
 
-    console.log("Server starting");
-
-    multi.on('change', function() {
-        console.log("Temp: ", this.thermometer.celsius, " Pressure: ", this.barometer.pressure, " Altitude: ", this.altimeter.meters);
-    });
-
+    // Configure server routes
     var app = express();
 
-    app.get('/', function(req, res) {
-        var board = {
-            "temp": multi.thermometer.celsius,
+    /**
+     * barometric sensor routes
+     */
+
+    // Returns the current values of the barometric sensor
+    app.get('/barometer', function(req, res) {
+        var values = {
+            "temperature": multi.thermometer.celsius,
             "pressure": multi.barometer.pressure,
             "altitude": multi.altimeter.meters
-        }
+        };
 
-        res.send(board);
+        res.json(values);
     });
 
+    // Start the server
     app.listen(3000, function() {
-        console.log("Server listening on port 3000");
+        console.log("Server listening on port 3000...");
     });
 });
