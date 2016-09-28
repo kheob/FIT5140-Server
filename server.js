56d23bfadb6502dd0a96c83e4379e9f05417cec7
Bai Chan Kheo <kheob@hotmail.com>
2016-09-28 16:38:12 +1000

    Test

--- server.js
+++ server.js
@@ -108,6 +108,8 @@ board.on('ready', function() {
                     // Turn into date object
                     var date = new Date(dateString);
 
+                    console.log("start date: ", startDate, " date: ", date, " end date: ", endDate);
+
                     // Check if it lies between the start and end dates
                     if (date >= startDate && date <= endDate) {
                         retrievedUpdates.push(barometerValues[dateString]);
