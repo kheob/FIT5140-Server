f512d6e8f35515e5bafd87030de3b3953acf5be2
Bai Chan Kheo <kheob@hotmail.com>
2016-09-28 16:48:19 +1000

    Test

--- server.js
+++ server.js
@@ -109,6 +109,8 @@ board.on('ready', function() {
                     // Turn into date object
                     var date = new Date(entry["date"]);
 
+                    console.log(entry["date"]);
+
                     console.log("start date: ", startDate, " date: ", date, " end date: ", endDate);
 
                     // Check if it lies between the start and end dates
